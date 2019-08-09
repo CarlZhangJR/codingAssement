5f0b82030c8418431c083ea9a227ddf984fa2c9b
CarlZhangJR <yushengjr002@gmail.com>
2019-08-09 09:37:07 -0400

    text error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchClickComponent } from './searchClick/search-click.component';
-import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { HttpClientModule } from '@angular/common/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
@@ -13,7 +12,6 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   declarations: [
     AppComponent,
     SearchClickComponent,
-    ProductDetailComponent,
     ProductListComponent
   ],
   imports: [
